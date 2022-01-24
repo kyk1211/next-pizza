b07802e19bf1195528e91664dd3ba733b55765fb
곽윤구 <dbsrndi123@naver.com>
2022-01-24 16:23:36 +0900

    deploy domain config change

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     domains: ['res.cloudinary.com'],
   },
   env: {
-    // DOMAIN: 'https://next-pizza-gilt.vercel.app',
-    DOMAIN: 'http://localhost:3000',
+    DOMAIN: 'https://next-pizza-gilt.vercel.app',
+    // DOMAIN: 'http://localhost:3000',
   },
 };
